5e226965fca98bfbfc240de5d17315bc76bc1390
lamcbaotoan <lamchubaotoan@gmail.com>
2025-06-26 10:35:33 +0700

    Đẩy file src vỏ hệ thống

---
+++ DA_CS403_N10/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Webebook
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-3.7.1.min.js",
+                DebugPath = "~/Scripts/jquery-3.7.1.js"
+            });
+        }
+    }
+}
